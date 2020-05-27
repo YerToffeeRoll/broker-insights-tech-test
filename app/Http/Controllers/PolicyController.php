0ac54e610341dbbbf2c92c6149f991ece174dd60
YerToffeeRoll <sam_killen_@hotmail.com>
2020-05-28 00:16:04 +0100

    Abstract the Policy api endpoints to a controller Edit the api routes file

---
+++ app/Http/Controllers/PolicyController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Policy;
+
+use Illuminate\Http\Request;
+
+class PolicyController extends Controller
+{
+  public function index()
+     {
+         return Policy::all();
+     }
+
+     public function show(Policy $policy)
+     {
+         return $policy;
+     }
+
+     public function store(Request $request)
+     {
+         $policy = Article::create($request->all());
+
+         return response()->json($policy, 201);
+     }
+
+     public function update(Request $request, Policy $policy)
+     {
+         $policy->update($request->all());
+
+         return response()->json($policy, 200);
+     }
+
+     public function delete(Policy $policy)
+     {
+         $policy->delete();
+
+         return response()->json(null, 204);
+     }
+}
