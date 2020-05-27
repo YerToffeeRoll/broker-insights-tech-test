5f39d787489fee1c8519b91ae3b6b4ab0e8a81d8
YerToffeeRoll <sam_killen_@hotmail.com>
2020-05-28 00:16:04 +0100

    Abstract the Policy api endpoints to a controller Edit the api routes file

--- routes/api.php
+++ routes/api.php
@@ -19,29 +19,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-Route::get('policies', function() {
-    // If the Content-Type and Accept headers are set to 'application/json',
-    // this will return a JSON structure. This will be cleaned up later.
-    return Policy::all();
-});
-
-Route::get('policies/{id}', function($id) {
-    return Policy::find($id);
-});
-
-Route::post('policies', function(Request $request) {
-    return Policy::create($request->all);
-});
-
-Route::put('policies/{id}', function(Request $request, $id) {
-    $policy = Policy::findOrFail($id);
-    $policy->update($request->all());
-
-    return $policy;
-});
-
-Route::delete('policies/{id}', function($id) {
-    Policy::find($id)->delete();
-
-    return 204;
-});
+Route::get('articles', 'ArticleController@index');
+Route::get('articles/{id}', 'ArticleController@show');
+Route::post('articles', 'ArticleController@store');
+Route::put('articles/{id}', 'ArticleController@update');
+Route::delete('articles/{id}', 'ArticleController@delete');
