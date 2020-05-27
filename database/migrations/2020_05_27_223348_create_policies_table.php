592b5dc29e5e632e2ec6cc9b136c81f26ab4c8e1
YerToffeeRoll <sam_killen_@hotmail.com>
2020-05-27 23:40:30 +0100

    -Created the policy migration

---
+++ database/migrations/2020_05_27_223348_create_policies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePoliciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('policies', function (Blueprint $table) {
+          $table->increments('id');
+          $table->string('customer_name');
+          $table->string('customer_address');
+          $table->integer('premium');
+          $table->string('policy_type');
+          $table->string('insurer_name');
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('policies');
+    }
+}
